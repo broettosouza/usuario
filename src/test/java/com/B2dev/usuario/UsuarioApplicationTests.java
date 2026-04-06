09d1889b8b4f45d4717450cdd9a44aae89e7a18a
Gabriel Broetto de souza <broetto.souza@hotmail.com>
2026-04-06 21:06:57 +0200

    commit inicial

---
+++ src/test/java/com/B2dev/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.B2dev.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
